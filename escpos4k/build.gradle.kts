9273eb85e68531e7ac93112af25a6687d8e799d2
Ondřej Karmazín <ondrej.karmazin@outlook.com>
2022-09-14 17:28:58 +0200

    Implement proper equals, hashCode, toString for all Commands

--- escpos4k/build.gradle.kts
+++ escpos4k/build.gradle.kts
@@ -99,4 +99,10 @@ android {
     sourceCompatibility = JavaVersion.VERSION_1_8
     targetCompatibility = JavaVersion.VERSION_1_8
   }
+
+  testOptions {
+    unitTests.all {
+      it.useJUnitPlatform()
+    }
+  }
 }
