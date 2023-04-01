e5bb2a18a95f349e96ab5f2291133d5f4e49dd9b
Ondřej Karmazín <ondrej.karmazin@outlook.com>
2023-04-01 16:01:04 +0200

    Remove unnecessary inline modifier
    Declutters build output.

--- escpos4k/src/commonTest/kotlin/cz/multiplatform/escpos4k/core/ArrowAssertions.kt
+++ escpos4k/src/commonTest/kotlin/cz/multiplatform/escpos4k/core/ArrowAssertions.kt
@@ -1,21 +1,23 @@
+@file:Suppress("unused")
+
 package cz.multiplatform.escpos4k.core
 
 import arrow.core.Either
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.types.shouldBeInstanceOf
 
-inline fun <A, B> Either<A, B>.shouldBeLeft(expected: A) {
+fun <A, B> Either<A, B>.shouldBeLeft(expected: A) {
   shouldBeInstanceOf<Either.Left<A>>().value shouldBe expected
 }
 
-inline fun <A, B> Either<A, B>.shouldBeLeft(): A {
+fun <A, B> Either<A, B>.shouldBeLeft(): A {
   return shouldBeInstanceOf<Either.Left<A>>().value
 }
 
-inline fun <A, B> Either<A, B>.shouldBeRight(expected: B) {
+fun <A, B> Either<A, B>.shouldBeRight(expected: B) {
   shouldBeInstanceOf<Either.Right<B>>().value shouldBe expected
 }
 
-inline fun <A, B> Either<A, B>.shouldBeRight(): B {
+fun <A, B> Either<A, B>.shouldBeRight(): B {
   return shouldBeInstanceOf<Either.Right<B>>().value
 }
