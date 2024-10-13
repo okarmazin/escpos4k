8f47188fd504fcaee814071c91f94bd6f8548c42
Ondřej Karmazín <ondrej.karmazin@outlook.com>
2024-10-13 20:07:01 +0200

    Simplify copyright headers by using SPDX-License-Identifier

--- escpos4k/src/commonMain/kotlin/cz/multiplatform/escpos4k/usb/UsbEndpoint.kt
+++ escpos4k/src/commonMain/kotlin/cz/multiplatform/escpos4k/usb/UsbEndpoint.kt
@@ -1,18 +1,4 @@
-/*
- *    Copyright 2024 escpos4k authors
- *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
- *
- *        http://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
- */
+// SPDX-License-Identifier: Apache-2.0
 
 package cz.multiplatform.escpos4k.usb
 
@@ -38,7 +24,7 @@ public data class UsbEndpoint(
   }
 
   public enum class Direction(internal val intValue: Int) {
-    @Suppress("MagicNumber")
+    @Suppress("MagicNumber") //
     In(128),
     Out(0),
   }
